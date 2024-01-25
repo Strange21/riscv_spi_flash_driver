52543a5415e349e6d8bc72a85760520d31afc731
Singh M Yashwanth (MS/ECL2) <iyg1kor@KOR-V-002EL.kor.apac.bosch.com>
2024-01-25 11:43:29 +0530

    target was removed

--- target/riscv64imac-unknown-none-elf/debug/deps/vajra_flash_driver-105a0d678af29d89.d
+++
@@ -1,8 +0,0 @@
-/home/iyg1kor/projects/Shakti/git_risc_v/riscv_spi_flash_driver/target/riscv64imac-unknown-none-elf/debug/deps/libvajra_flash_driver-105a0d678af29d89.rmeta: src/main.rs src/common.rs src/spi.rs src/uart.rs
-
-/home/iyg1kor/projects/Shakti/git_risc_v/riscv_spi_flash_driver/target/riscv64imac-unknown-none-elf/debug/deps/vajra_flash_driver-105a0d678af29d89.d: src/main.rs src/common.rs src/spi.rs src/uart.rs
-
-src/main.rs:
-src/common.rs:
-src/spi.rs:
-src/uart.rs:
